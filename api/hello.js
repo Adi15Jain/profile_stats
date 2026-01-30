afdae4f7d8142e7872de725e8fbd4a01fbc6482d
Adi Jain <adi1510jain@gmail.com>
2026-01-30 13:28:10 +0530

    Initial Node.js Vercel API

--- api/hello.js
+++ api/hello.js
@@ -1,9 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    status: 'ok',
-    message: 'API is working',
-  });
-}
 import { fetchWakaTimeLast7Days } from '../lib/wakatime.js';
 import { renderWakaTimeSVG } from '../lib/svg.js';
 
@@ -16,14 +10,16 @@ export default async function handler(req, res) {
     res.setHeader('Cache-Control', 'public, max-age=21600');
     res.status(200).send(svg);
   } catch (err) {
+    console.error('WakaTime error:', err);
+
     res.setHeader('Content-Type', 'image/svg+xml');
     res.status(200).send(`
-<svg width="420" height="80" xmlns="http://www.w3.org/2000/svg">
-  <rect width="100%" height="100%" rx="12" fill="#0d1117"/>
-  <text x="20" y="45" fill="#c9d1d9">
-    WakaTime stats unavailable
-  </text>
-</svg>
-`);
+      <svg width="420" height="80" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" rx="12" fill="#0d1117"/>
+        <text x="20" y="45" fill="#c9d1d9">
+          WakaTime stats unavailable
+        </text>
+      </svg>
+    `);
   }
 }
